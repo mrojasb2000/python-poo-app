1cdebf33e0481a343e37db29c0e0b74decc6d5f0
mauricio rojas <mrojasb2000@gmail.com>
2023-08-16 22:20:14 -0400

    feat: initial commit

---
+++ celular.py
@@ -0,0 +1,5 @@
+class Celular:
+    def __init__(self, marca, model):
+        self.marca = marca
+        self.model = model
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from celular import Celular
+
+samsungA32 = Celular("Samsung", "A32")
+appleIphone15 = Celular("Apple", "Iphone 15")
+
+print(samsungA32.marca)
+print(samsungA32.model)
+
+
+print(appleIphone15.marca)
+print(appleIphone15.model)
